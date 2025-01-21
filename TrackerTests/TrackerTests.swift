48d74090476c66ae02f0bdc9b4473c8028734698
Vegicode <vadimegiazarov@mail.ru>
2025-01-21 11:43:32 +0300

    Убрал ошибки

--- TrackerTests/TrackerTests.swift
+++ TrackerTests/TrackerTests.swift
@@ -5,12 +5,30 @@
 //  Created by Mac on 20.01.2025.
 //
 
-import Testing
+import XCTest
+import SnapshotTesting
+@testable import Tracker
 
-struct TrackerTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+final class TrackersBackgroundTests: XCTestCase {
+    override func setUp() {
+        super.setUp()
+        SnapshotTesting.isRecording = false
     }
 
+    func testTrackersViewControllerAppearance_LightAndDark() {
+        let trackersVC = TaskListViewController(viewModel: TaskListViewModel())
+        trackersVC.loadViewIfNeeded()
+
+        trackersVC.view.frame = CGRect(x: 0, y: 0, width: 430, height: 932)
+
+        trackersVC.view.backgroundColor = .ccWhite
+        assertSnapshot(of: trackersVC.view, as: .image, named: "Light")
+
+        trackersVC.view.backgroundColor = .red
+        assertSnapshot(of: trackersVC.view, as: .image, named: "Light_Modified")
+
+        let traitsDark = UITraitCollection(userInterfaceStyle: .dark)
+        trackersVC.view.backgroundColor = .ccWhite
+        assertSnapshot(of: trackersVC.view, as: .image(traits: traitsDark), named: "Dark")
+    }
 }
